e6f0286b16ff2b49cef619866caad8896008db2a
Ozxx <dimonkozlovskiy@gmail.com>
2018-08-14 18:38:25 +0300

    delete old store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
-
+import store from "./store";
 import { App } from "./components/App";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("root")
 );
--- src/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore, Store } from 'redux';
-import * as appReducers  from './reducers/index';
-import ColumnReducers  from './reducers/column';
-
-const initialState = {};
-
-const store: Store<any> = createStore(ColumnReducers, initialState);
-
-export default store;
